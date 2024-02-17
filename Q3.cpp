0c0fef47174996320002a1818e4f3f5c625a7f1b
gaurav22121 <160212426+gaurav22121@users.noreply.github.com>
2024-02-17 17:31:52 +0530

    BASIC_ASSIGNMENT

---
+++ Q3.cpp
@@ -0,0 +1,9 @@
+//How much space does the following data types take?
+// (int)
+// (bool)
+// (float)
+
+//-------- ANS---------- :
+// int  : 4 bytes
+//bool  : 1 bytes
+//float : 4 bytes
---
+++ Q6.cpp
@@ -0,0 +1,10 @@
+//How many of these can be a variable name ?
+// 1)01Pwskills
+// 2)_FLOAT
+// 3)int
+// 4)FLOAT
+// 5)You will succeed
+
+//--------ANS--------
+// 2) _FLOAT
+// 4) FLOAT
